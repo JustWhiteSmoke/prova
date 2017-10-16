681cc01301a8ef738b9e1053e7ebdb0d2bbb5e0c
Alunno <Alunno@PC-3-INF4>
2017-10-16 12:31:52 +0200

    cgjhcgjhc

--- prova/src/prova/provaprova123sasasa.java
+++ prova/src/prova/provaprova123sasasa.java
@@ -2,7 +2,8 @@ package prova;
 
 public class provaprova123sasasa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World :)");
 
 	}
